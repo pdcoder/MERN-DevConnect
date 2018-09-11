350b2ed16389bca43d891f93d45574eef6fc8b47
Prakash <prakashdas1998.pd@gmail.com>
2018-09-11 22:44:51 +0530

    Added validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,9 +7,11 @@ const Profile = require('../../models/Profile');
 router.get('/test',(req,res)=> res.json({msg : 'Profile Works'}));
 
 router.get('/',passport.authenticate('jwt', {session: false}), (req,res) => {
+   const errors = {};
     Profile.findOne({user : req.user.id})
     .then(profile => {
         if(!profile){
+            errors.noprofile = "There is no profile for this user";
             return res.status(404).json()
         }
     });
