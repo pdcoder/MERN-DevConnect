5a30cf163d78b23db49cb9533e7c40cfc9a08c28
Prakash <prakashdas1998.pd@gmail.com>
2018-09-11 19:33:26 +0530

    Initialized app

---
+++ server.js
@@ -0,0 +1,35 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//db config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongo
+mongoose.connect(db).then(()=>
+    console.log("Mongoose connected"))
+    .catch(err => console.log(err));
+
+//passport
+app.use(passport.initialize());
+
+//passport config
+require('./configs/passport')(passport);
+
+//use routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=> console.log("Server running on port ${port}"));
