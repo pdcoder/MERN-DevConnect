9baeafcb3ac3fc046997800da1afabd8f6ace390
Prakash <prakashdas1998.pd@gmail.com>
2018-09-18 17:45:34 +0530

    Added keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  mongoURI: 'mongodb://pdcoder:pdcoder1!@ds111895.mlab.com:11895/pdcoder',
   secretOrKey: 'secret'
 };
