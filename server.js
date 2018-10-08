09f3bd943f35a2b22a5899ace5528a5b8ad96562
Prakash <prakashdas1998.pd@gmail.com>
2018-10-09 01:11:07 +0530

    Modified project structure

--- server.js
+++ server.js
@@ -37,10 +37,9 @@ app.use('/api/posts', posts);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
-  app.use("/", express.static(path.join(__dirname,"client","build")));
+  app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname,'build', 'index.html'));
   });
 }
 
