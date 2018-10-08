aacf91c77ec4aa4903e9e978ea4919d193a655dc
Prakash <prakashdas1998.pd@gmail.com>
2018-10-08 17:21:26 +0530

    Added fix

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express  = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -9,27 +10,39 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//db config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//connect to mongo
-mongoose.connect(db).then(()=>
-    console.log("Mongoose connected"))
-    .catch(err => console.log(err));
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-//passport
+// Passport middleware
 app.use(passport.initialize());
 
-//passport config
+// Passport Config
 require('./config/passport')(passport);
 
-//use routes
-app.use('/api/users',users);
-app.use('/api/profile',profile);
-app.use('/api/posts',posts);
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5000;
-app.listen(port, ()=> console.log("Server running on port ${port}"));
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5500;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
