04f9a1109208f6fa0591659788991f2d53aef825
Prakash <prakashdas1998.pd@gmail.com>
2018-10-09 01:19:56 +0530

    Modified project structure

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
