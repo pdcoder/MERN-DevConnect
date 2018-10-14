297bfd850c716a0ace73a5eb821efac9141997cb
Prakash <prakashdas1998.pd@gmail.com>
2018-10-14 22:41:02 +0530

    Finished

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,12 +5,12 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+const User = require('../../models/User');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
-const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
