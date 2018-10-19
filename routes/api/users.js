615ca65d6af319bd2ce81b3146834a95e11b1d93
Prakash Das <prakashdas1998.pd@gmail.com>
2018-10-19 22:52:27 +0530

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,25 +6,16 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
-// Load Input Validation
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
-// Load User model
 const User = require('../../models/user');
 
-// @route   GET api/users/test
-// @desc    Tests users route
-// @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
-// @route   POST api/users/register
-// @desc    Register user
-// @access  Public
 router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
-  // Check Validation
   if (!isValid) {
     return res.status(400).json(errors);
   }
@@ -35,9 +26,9 @@ router.post('/register', (req, res) => {
       return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: '200', // Size
-        r: 'pg', // Rating
-        d: 'mm' // Default
+        s: '200', 
+        r: 'pg', 
+        d: 'mm' 
       });
 
       const newUser = new User({
@@ -61,13 +52,9 @@ router.post('/register', (req, res) => {
   });
 });
 
-// @route   GET api/users/login
-// @desc    Login User / Returning JWT Token
-// @access  Public
 router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
-  // Check Validation
   if (!isValid) {
     return res.status(400).json(errors);
   }
@@ -75,21 +62,16 @@ router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  // Find user by email
   User.findOne({ email }).then(user => {
-    // Check for user
     if (!user) {
       errors.email = 'User not found';
       return res.status(404).json(errors);
     }
 
-    // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        // User Matched
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
 
-        // Sign Token
         jwt.sign(
           payload,
           keys.secretOrKey,
@@ -109,9 +91,6 @@ router.post('/login', (req, res) => {
   });
 });
 
-// @route   GET api/users/current
-// @desc    Return current user
-// @access  Private
 router.get(
   '/current',
   passport.authenticate('jwt', { session: false }),
